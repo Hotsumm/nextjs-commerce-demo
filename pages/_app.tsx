d1392ae92e3aa08172d4ff08f079acc0e5a0d532
SungYeolha <tjddufgk@gmail.com>
2022-05-17 04:09:51 +0900

    absolute path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import { Global } from '@emotion/react';
-import { GlobalStyles } from '../styles/globalStyle';
+import { GlobalStyles } from 'styles/globalStyle';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
