5f11e915193af74e2ae1ceb312147c6b20b45a4d
SungYeolha <tjddufgk@gmail.com>
2022-05-17 20:35:11 +0900

    add Layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from 'next/app';
 import { Global } from '@emotion/react';
 import { GlobalStyles } from 'styles/globalStyle';
+import Layout from 'src/components/layouts/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Global styles={GlobalStyles} />
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
