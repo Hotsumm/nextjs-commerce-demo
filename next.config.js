8c7f7649166db2bfef35ee3269bcfe4297c563ac
SungYeolha <tjddufgk@gmail.com>
2022-05-18 22:04:53 +0900

    big commerce setting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const commerce = require('./commerce.config.json');
 const nextConfig = {
   reactStrictMode: true,
-}
+  commerce,
+  images: {
+    domains: ['cdn11.bigcommerce.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
